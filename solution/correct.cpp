48d229773ffc33859efa952faa000a49fe1f9f4a
LeoChin <6397leo@gmail.com>
2025-05-05 17:34:08 +0800

    20250505 Explanation of code

--- solution/correct.cpp
+++ solution/correct.cpp
@@ -2,6 +2,14 @@
 #include <stdlib.h>
 #include <assert.h>
 
+/*
+ * Maintaining one min heap and one max heap doesn't fulfill the memory constraint. 
+ * In each node of each heap, it is necessary to store job and priority.
+ * There are at most 10^6 jobs, so this implementation uses at least 4B * 2 * 2 * 10^6 = 16MB. 
+ * In addition, memory to maintain the connection between the heaps is not counted yet.
+ * So this implementation is not feasible.
+ */
+
 #define MAXSZ 1000000
 
 typedef struct job {
@@ -18,7 +26,7 @@ void swap(Job *a, Job *b){
     *b = temp;
 }
 
-int isMinLevel(int idx){
+int isMinLevel(int idx){    // Time complexity: O(log |idx|)
     idx++;
     int level = 0;
     while (idx > 0){
@@ -28,11 +36,12 @@ int isMinLevel(int idx){
     return level % 2;
 }
 
-void heapify(int m){
-    while (2 * m + 1 < size){
+void heapify(int m){        // Time complexity: O(log(size))
+    while (2 * m + 1 < size){   // O(log(size))
         int i = m;
         if (isMinLevel(i)){
             int minIdx = -1, minPrio = 2147483647;
+            // Find the minimum among the children and grandchildren
             int idxArr[6] = {2 * i + 1, 2 * i + 2, 4 * i + 3, 4 * i + 4, 4 * i + 5, 4 * i + 6};
             for (int j=0; j<6; j++){
                 if (idxArr[j] < size && heap[idxArr[j]].priority < minPrio){
@@ -45,6 +54,7 @@ void heapify(int m){
             if (heap[m].priority < heap[i].priority){
                 swap(&heap[m], &heap[i]);
                 if (m >= 4 * i + 3){
+                    // Is grandchild
                     if (heap[m].priority > heap[(m - 1) / 2].priority){
                         swap(&heap[m], &heap[(m - 1) / 2]);
                     }
@@ -55,6 +65,7 @@ void heapify(int m){
         }
         else {
             int maxIdx = -1, maxPrio = -2147483648;
+            // Find the maximum among the children and grandchildren
             int idxArr[6] = {2 * i + 1, 2 * i + 2, 4 * i + 3, 4 * i + 4, 4 * i + 5, 4 * i + 6};
             for (int j=0; j<6; j++){
                 if (idxArr[j] < size && heap[idxArr[j]].priority > maxPrio){
@@ -67,6 +78,7 @@ void heapify(int m){
             if (heap[m].priority > heap[i].priority){
                 swap(&heap[m], &heap[i]);
                 if (m >= 4 * i + 3){
+                    // Is grandchild
                     if (heap[m].priority < heap[(m - 1) / 2].priority){
                         swap(&heap[m], &heap[(m - 1) / 2]);
                     }
@@ -78,7 +90,7 @@ void heapify(int m){
     }
 }
 
-void BubbleMax(int idx){
+void BubbleMax(int idx){    // Time complexity: O(log(size))
     int grandparentIdx = (idx - 3) / 4;
     while (idx > 2 && heap[idx].priority > heap[grandparentIdx].priority){
         swap(&heap[idx], &heap[grandparentIdx]);
@@ -87,7 +99,7 @@ void BubbleMax(int idx){
     }
 }
 
-void BubbleMin(int idx){
+void BubbleMin(int idx){    // Time complexity: O(log(size))
     int grandparentIdx = (idx - 3) / 4;
     while (idx > 2 && heap[idx].priority < heap[grandparentIdx].priority){
         swap(&heap[idx], &heap[grandparentIdx]);
@@ -96,35 +108,35 @@ void BubbleMin(int idx){
     }
 }
 
-void BubbleUp(int idx){
+void BubbleUp(int idx){     // Time complexity: O(log(size))
     if (idx > 0){
         int parentIdx = (idx - 1) / 2;
         if (isMinLevel(idx)){
             if (heap[idx].priority > heap[parentIdx].priority){
                 swap(&heap[idx], &heap[parentIdx]);
-                BubbleMax(parentIdx);
+                BubbleMax(parentIdx);   // O(log(size))
             }
             else {
-                BubbleMin(idx);
+                BubbleMin(idx);         // O(log(size))
             }
         }
         else {
             if (heap[idx].priority < heap[parentIdx].priority){
                 swap(&heap[idx], &heap[parentIdx]);
-                BubbleMin(parentIdx);
+                BubbleMin(parentIdx);   // O(log(size))
             }
             else {
-                BubbleMax(idx);
+                BubbleMax(idx);         // O(log(size))
             }
         }
     }
 }
 
-int FindMin(){
+int FindMin(){              // Time complexity: O(1)
     return (size > 0)? 0: -1;
 }
 
-int FindMax(){
+int FindMax(){              // Time complexity: O(1)
     if (size == 0) return -1;
     if (size < 3) return (size - 1);
     return (heap[1].priority > heap[2].priority)? 1: 2;
@@ -137,34 +149,34 @@ int main(){
     int type, job_id, priority;
     for (int i=0; i<N; i++){
         scanf("%d", &type);
-        if (type == 1){
+        if (type == 1){             // Time complexity of insertion: O(log(size))
             scanf("%d %d", &job_id, &priority);
             heap[size].job_id = job_id;
             heap[size].priority = priority;
             size++;
             
-            BubbleUp(size - 1);
+            BubbleUp(size - 1);     // O(log(size))
             printf("%d jobs waiting\n", size);
         }
-        else if (type == 2){
-            int maxIdx = FindMax();
+        else if (type == 2){        // Time complexity of deleteMax: O(log(size))
+            int maxIdx = FindMax(); // O(1)
             if (maxIdx == -1){
                 printf("no job in queue\n");
                 continue;
             }
             printf("job %d with priority %d completed\n", heap[maxIdx].job_id, heap[maxIdx].priority);
             heap[maxIdx] = heap[--size];
-            heapify(maxIdx);
+            heapify(maxIdx);        // O(log(size))
         }
-        else {
-            int minIdx = FindMin();
+        else {                      // Time complexity of deleteMin: O(log(size))
+            int minIdx = FindMin(); // O(1)
             if (minIdx == -1){
                 printf("no job in queue\n");
                 continue;
             }
             printf("job %d with priority %d dropped\n", heap[minIdx].job_id, heap[minIdx].priority);
             heap[minIdx] = heap[--size];
-            heapify(minIdx);
+            heapify(minIdx);        // O(log(size))
         }
     }
 
